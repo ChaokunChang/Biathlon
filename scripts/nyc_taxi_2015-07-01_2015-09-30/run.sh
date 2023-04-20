2ed5310bc057d24a76bedc0742d0cb9862e446b2
ckchang <ckchang@cse.cuhk.edu.hk>
2023-04-20 23:06:27 +0800

    move core code to folder apxinfer under home

--- scripts/nyc_taxi_2015-07-01_2015-09-30/run.sh
+++ scripts/nyc_taxi_2015-07-01_2015-09-30/run.sh
@@ -6,24 +6,37 @@ else
     model="lgbm"
 fi
 
-cfg1="--task day_of_week --keycol day_of_year --sort_by day_of_year --target day_of_week --sql_templates_file day_of_week/templates.sql --model_type classifier --model_name $model --multi_class"
-cfg2="--task is_weekend --keycol day_of_year --sort_by day_of_year --target is_weekend --sql_templates_file is_weekend/templates.sql --model_type classifier --model_name $model"
-cfg3="--task hour_of_day --keycol hourstamp --sort_by hourstamp --target hour_of_day --sql_templates_file hour_of_day/templates.sql --model_type classifier --model_name $model --multi_class"
-cfg4="--task is_night --keycol hourstamp --sort_by hourstamp --target is_night --sql_templates_file is_night/templates.sql --model_type classifier --model_name $model"
-cfgs="$cfg1|$cfg2|$cfg3|$cfg4"
+cur_dir=$(pwd)
+scripts_dir=$(dirname $cur_dir)
+home_dir=$(dirname $scripts_dir)
+apxinfer_dir=$home_dir/apxinfer
+
+data_name="nyc_taxi_2015-07-01_2015-09-30"
+data_dir=$home_dir/data/$data_name
+
+# classification tasks
+cfg1="--data $data_name --task day_of_week --keycol day_of_year --sort_by day_of_year --target day_of_week --sql_templates_file day_of_week/templates.sql --model_type classifier --model_name $model --multi_class"
+cfg2="--data $data_name --task is_weekend --keycol day_of_year --sort_by day_of_year --target is_weekend --sql_templates_file is_weekend/templates.sql --model_type classifier --model_name $model"
+cfg3="--data $data_name --task hour_of_day --keycol hourstamp --sort_by hourstamp --target hour_of_day --sql_templates_file hour_of_day/templates.sql --model_type classifier --model_name $model --multi_class"
+cfg4="--data $data_name --task is_night --keycol hourstamp --sort_by hourstamp --target is_night --sql_templates_file is_night/templates.sql --model_type classifier --model_name $model"
+
+# regression tasks
+cfg5="--data $data_name --task fare_prediction_2015-08-01_2015-08-15_10000 --keycol trip_id --sort_by pickup_datetime --target fare_amount --sql_templates_file fare_prediction_2015-08-01_2015-08-15_10000/templates.sql --model_type regressor --model_name $model"
+
+cfgs="$cfg1|$cfg2|$cfg3|$cfg4|$cfg5"
 
 cfg=$(echo $cfgs | cut -d'|' -f$cfgid)
 echo $cfg
 
-python prepares.py $cfg
+python $apxinfer_dir/prepares.py $cfg
 
-python fextractor.py $cfg --sample 0
-python fextractor.py $cfg --sample 0.01
-python fextractor.py $cfg --sample 0.1
-python fextractor.py $cfg --sample 0.5
+python $apxinfer_dir/fextractor.py $cfg --sample 0
+python $apxinfer_dir/fextractor.py $cfg --sample 0.01
+python $apxinfer_dir/fextractor.py $cfg --sample 0.1
+python $apxinfer_dir/fextractor.py $cfg --sample 0.5
 
-python pipeline.py $cfg
+python $apxinfer_dir/pipeline.py $cfg
 
-python test_pipeline.py $cfg --sample 0.01
-python test_pipeline.py $cfg --sample 0.1
-python test_pipeline.py $cfg --sample 0.5
+python $apxinfer_dir/test_pipeline.py $cfg --sample 0.01
+python $apxinfer_dir/test_pipeline.py $cfg --sample 0.1
+python $apxinfer_dir/test_pipeline.py $cfg --sample 0.5

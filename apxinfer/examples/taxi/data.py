1c45e580042c859b22016733d987723918e79e9b
ckchang@numa <ckchang@cse>
2023-07-07 07:39:24 -0400

    taxi trips table should have more index.
    ORDER BY (pickup_ntaname, dropoff_ntaname,         passenger_count, pickup_datetime)

--- apxinfer/examples/taxi/data.py
+++ apxinfer/examples/taxi/data.py
@@ -67,7 +67,8 @@ class TaxiTripIngestor(XIPDataIngestor):
                     pid UInt32 -- partition key, used for sampling
                 ) ENGINE = MergeTree()
                 PARTITION BY pid
-                ORDER BY (pickup_datetime)
+                ORDER BY (pickup_ntaname, dropoff_ntaname,
+                        passenger_count, pickup_datetime)
                 SETTINGS index_granularity = 32
             """
         self.db_client.command(sql)

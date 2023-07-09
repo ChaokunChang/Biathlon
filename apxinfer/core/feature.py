1. bootstrapping 1000x is very expensive, causing no speedup, we use 100 instead.  Latter we may also consider to optimize bootstrapping. 2. fix float precision error with function is_same_float()

apxinfer/core/feature.py
@@ -12,7 +12,7 @@ from apxinfer.core.utils import (
     XIPQueryConfig,
     QueryCostEstimation,
 )
-from apxinfer.core.utils import merge_fvecs
+from apxinfer.core.utils import merge_fvecs, is_same_float
 from apxinfer.core.query import XIPQuery
 
 fcache_manager = CacheManager(
@@ -23,7 +23,7 @@ logging.basicConfig(level=logging.INFO)
 
 class FEstimatorHelper:
     min_cnt = 30
-    bs_nsamp: int = 1000
+    bs_nsamp: int = 100
     bias_correction: bool = True
 
     def estimate_any(
@@ -32,7 +32,7 @@ class FEstimatorHelper:
         func: Callable,
         tsize: int,
     ) -> XIPFeatureVec:
-        if p >= 1.0:
+        if is_same_float(p, 1.0):
             features = func(data)
             fnames = [f"{func.__name__}_f{i}" for i in range(features.shape[0])]
             return XIPFeatureVec(
@@ -103,7 +103,7 @@ class FEstimatorHelper:
             return np.var(data, axis=0, ddof=ddof)
 
     def fstds_crop(fstds: np.ndarray, p: float, card: int) -> np.ndarray:
-        if p >= 1.0:
+        if is_same_float(p, 1.0):
             return np.zeros_like(fstds)
         elif card < FEstimatorHelper.min_cnt:
             return 1e9 * np.ones_like(fstds)

apxinfer/core/model.py
@@ -254,7 +254,10 @@ def evaluate_classifier(
     f1 = metrics.f1_score(y, y_pred)
     prec = metrics.precision_score(y, y_pred)
     rec = metrics.recall_score(y, y_pred)
-    auc = metrics.roc_auc_score(y, y_pred)
+    try:
+        auc = metrics.roc_auc_score(y, y_pred)
+    except ValueError:
+        auc = np.nan
     return ClassifierEvaluation(
         acc=acc, f1=f1, prec=prec, rec=rec, auc=auc, size=len(y), time=et - st
     )

apxinfer/core/pipeline.py
@@ -7,7 +7,7 @@ from apxinfer.core.utils import XIPRequest, XIPQueryConfig
 from apxinfer.core.utils import XIPFeatureVec, XIPPredEstimation
 
 # from apxinfer.core.utils import QueryCostEstimation
-from apxinfer.core.utils import XIPPipelineSettings
+from apxinfer.core.utils import XIPPipelineSettings, is_same_float
 from apxinfer.core.feature import XIPFeatureExtractor
 from apxinfer.core.model import XIPModel
 from apxinfer.core.prediction import XIPPredictionEstimator
@@ -36,7 +36,9 @@ class XIPPipeline:
         self.logger = logging.getLogger("XIPPipeline")
 
     def is_exact_pred(self, pred: XIPPredEstimation) -> bool:
-        return pred["pred_error"] == 0.0 and pred["pred_conf"] == 1.0
+        return is_same_float(pred["pred_error"], 0.0) and is_same_float(
+            pred["pred_conf"], 1.0
+        )
 
     def meet_termination_condition(
         self,
@@ -47,7 +49,7 @@ class XIPPipeline:
     ) -> bool:
         if self.is_exact_pred(pred):
             return True
-        elif np.all([qcfg["qsample"] >= 1.0 for qcfg in qcfgs]):
+        elif np.all([is_same_float(qcfg["qsample"], 1.0) for qcfg in qcfgs]):
             return True
         if self.settings.termination_condition == "min_max":
             return (

apxinfer/core/query.py
@@ -3,7 +3,13 @@ import logging
 import numpy as np
 from beaker.cache import CacheManager
 
-from apxinfer.core.utils import XIPFeatureVec, XIPRequest, XIPQType, XIPQueryConfig
+from apxinfer.core.utils import (
+    XIPFeatureVec,
+    XIPRequest,
+    XIPQType,
+    XIPQueryConfig,
+    is_same_float,
+)
 from apxinfer.core.data import XIPDataLoader
 
 fcache_manager = CacheManager(
@@ -42,7 +48,7 @@ class XIPQuery:
     ) -> XIPFeatureVec:
         qsample = qcfg["qsample"]
         fvals = np.zeros(len(self.fnames))
-        if qsample >= 1.0:
+        if is_same_float(qsample, 1.0):
             self.logger.warning(f"no data for {request} in {self.qname}")
             fests = np.zeros(len(self.fnames))
         else:

apxinfer/examples/ccfraud/query.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from apxinfer.core.utils import XIPQueryConfig, XIPFeatureVec, XIPQType
-from apxinfer.core.utils import merge_fvecs
+from apxinfer.core.utils import merge_fvecs, is_same_float
 from apxinfer.core.data import DBHelper
 from apxinfer.core.query import XIPQuery
 from apxinfer.core.feature import FEstimatorHelper
@@ -125,7 +125,9 @@ class CCFraudQ2(XIPQuery):
         fcols = self.fnames
         fvals = self.data_loader.load_data(request, qcfg, fcols)
         for i in range(len(self.num_fnames), len(fvals)):
-            fvals[i] = self.embeddings[self.cat_fnames[i - len(self.num_fnames)]].get(fvals[i], 0)
+            fvals[i] = self.embeddings[self.cat_fnames[i - len(self.num_fnames)]].get(
+                fvals[i], 0
+            )
         fests = np.zeros_like(fvals)
         fdists = ["normal"] * len(fvals)
         return XIPFeatureVec(
@@ -162,7 +164,7 @@ class CCFraudQ3(XIPQuery):
 
         if req_data is None or len(req_data) == 0:
             fvals = np.zeros(len(self.fnames))
-            if qsample >= 1.0:
+            if is_same_float(qsample, 1.0):
                 self.logger.warning(f"no data for {request}")
                 fests = np.zeros(len(self.fnames))
             else:
@@ -209,7 +211,7 @@ class CCFraudQ4(XIPQuery):
 
         if req_data is None or len(req_data) == 0:
             fvals = np.zeros(len(self.fnames))
-            if qsample >= 1.0:
+            if is_same_float(qsample, 1.0):
                 self.logger.warning(f"no data for {request}")
                 fests = np.zeros(len(self.fnames))
             else:
@@ -256,7 +258,7 @@ class CCFraudQ5(XIPQuery):
 
         if req_data is None or len(req_data) == 0:
             fvals = np.zeros(len(self.fnames))
-            if qsample >= 1.0:
+            if is_same_float(qsample, 1.0):
                 self.logger.warning(f"no data for {request}")
                 fests = np.zeros(len(self.fnames))
             else:

apxinfer/examples/traffic/query.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from apxinfer.core.utils import XIPQueryConfig, XIPFeatureVec, XIPQType
-from apxinfer.core.utils import merge_fvecs
+from apxinfer.core.utils import merge_fvecs, is_same_float
 from apxinfer.core.data import DBHelper
 from apxinfer.core.query import XIPQuery
 from apxinfer.core.feature import FEstimatorHelper
@@ -19,7 +19,9 @@ def get_borough_embedding() -> dict:
     boroughs: pd.DataFrame = db_client.query_df(
         "select distinct borough from xip.traffic order by borough"
     )
-    borough_map = {borough: i + 1 for i, borough in enumerate(boroughs["borough"].values)}
+    borough_map = {
+        borough: i + 1 for i, borough in enumerate(boroughs["borough"].values)
+    }
     return borough_map
 
 
@@ -116,7 +118,7 @@ class TrafficQP2(XIPQuery):
 
         if req_data is None or len(req_data) == 0:
             fvals = np.zeros(len(self.fnames))
-            if qsample >= 1.0:
+            if is_same_float(qsample, 1.0):
                 self.logger.warning(f"no data for {request}")
                 fests = np.zeros(len(self.fnames))
             else:
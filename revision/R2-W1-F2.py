517fb9d51c199ad369768aa9342d9887ca4548df
ckchang <ckchang@cse.cuhk.edu.hk>
2024-04-09 20:06:40 +0800

    update 2.1.2

--- revision/R2-W1-F2.py
+++ revision/R2-W1-F2.py
@@ -18,6 +18,7 @@ from sklearn import metrics
 from scipy import stats
 import warnings
 import gc
+import statsmodels.api as sm
 
 from SALib.sample import sobol as sobol_sample
 from SALib.analyze import sobol as sobol_analyze
@@ -36,28 +37,37 @@ from apxinfer.simulation import utils as simutils
 
 if __name__ == "__main__":
     x = 100
-    operator = 'median'
+    operator = "median"
+    # dsize = 2000_000 + 1
     dsize = 1000_000
     dbseed = 0
-    ddist = 'zipf'
+    ddist = "zipf"
     dist_arg = 1.05
-    population = simutils.generate_synthetic_data(x, operator, dsize=dsize, seed=dbseed, ddist=ddist, arg=dist_arg)
+    population = simutils.generate_synthetic_data(
+        x, operator, dsize=dsize, seed=dbseed, ddist=ddist, arg=dist_arg
+    )
 
+    # dsize = 2000_000 + 1
     # datas = []
     # rng = np.random.default_rng(dbseed)
-    # for i in range(10):
-    #     loc = rng.uniform(-10, 10)
-    #     scale = rng.uniform(1, 5)
-    #     mode_samples = np.random.normal(loc=loc, scale=scale, size=dsize // 10)
-    #     datas.append(mode_samples)
+    # nmodals = 2
+    # for i in range(nmodals):
+    #     loc = rng.uniform(-10000, 10000)
+    #     scale = rng.uniform(1, 10000)
+    #     part_samples = rng.normal(loc=loc, scale=scale, size=dsize // nmodals)
+    #     # a = rng.uniform(0.5, 2)
+    #     # b = rng.uniform(3, 10)
+    #     # part_samples = rng.beta(a, b, size=dsize // nmodals)
+    #     datas.append(part_samples)
     # population = np.concatenate(datas)
 
     sr = 0.01
     sample_size = int(sr * len(population))
-    print(f'Population size: {len(population)}, Sample size: {sample_size}')
+    print(f"Population size: {len(population)}, Sample size: {sample_size}")
 
+    nseeds = 1000
     median_list = []
-    for seed in range(1000):
+    for seed in range(nseeds):
         rng = np.random.default_rng(seed)
         samples = rng.choice(population, sample_size, replace=False)
         median_list.append(np.median(samples))
@@ -65,58 +75,150 @@ if __name__ == "__main__":
     error_list = median_list - np.median(population)
     abserror_list = np.abs(error_list)
 
-    print(f'Median of population: {np.median(population)}')
-    print(f'Mean of medians: {np.mean(median_list)}')
-    print(f'Std of medians: {np.std(median_list)}')
+    print(f"Median of population: {np.median(population)}")
+    print(f"Mean of medians: {np.mean(median_list)}")
+    print(f"Std of medians: {np.std(median_list)}")
 
     # test normality of error_list
     _, pval = stats.shapiro(error_list)
-    print(f'pval={pval}')
+    print(f"error_list normality pval={pval}")
 
-    bs_seed = 0
-    rng = np.random.default_rng(bs_seed)
+    rng = np.random.default_rng(0)
     samples = rng.choice(population, sample_size, replace=False)
     sample_median = np.median(samples)
+
+    # nresamples = nseeds
+    nresamples = 100
     resamples = []
-    for i in range(1000):
+    bs_seed = 0
+    rng = np.random.default_rng(bs_seed)
+    for i in range(nresamples):
         resamples.append(rng.choice(samples, sample_size, replace=True))
     resamples = np.array(resamples)
 
     resample_medians = np.median(resamples, axis=1)
-    # bias correction
-    bias = np.mean(resample_medians - sample_median)
-    resample_medians -= bias
-
-    resample_errors = resample_medians - np.median(population)
+    resample_errors = resample_medians - sample_median
     resample_abserrors = np.abs(resample_errors)
 
+    # bias correction
+    mean_resample_medians = np.mean(resample_medians)
+    bias = mean_resample_medians - sample_median
+    bs_median_estimation = sample_median + bias
+
+    print(f'bias                 : {bias}')
+    print(f"Median of sample     : {sample_median}")
+    print(f"bs_median_estimation : {bs_median_estimation}")
+
+    normal_bs_loc = 0
+    normal_bs_std = np.std(resample_medians, ddof=1)
+    normal_bs_errors = stats.norm.rvs(loc=normal_bs_loc, scale=normal_bs_std, size=10000)
+
+    # rng = np.random.default_rng(bs_seed)
+    # tmp_data = (samples, )
+    # scipy_bs_result = stats.bootstrap(tmp_data, np.median, n_resamples=nresamples, random_state=rng)
+    # scipy_bs_dist = scipy_bs_result.bootstrap_distribution
+    # scipy_bs_errors = scipy_bs_dist - sample_median
+
     # plot distribution of error_list
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    fig, axes = plt.subplots(2, 2, figsize=(12, 12))
     axes = axes.flatten()
+
     ax = axes[0]
-    sns.histplot(error_list, kde=True, label='Real Distribution', color='blue', alpha=0.5, bins=50, stat='density', ax=ax)
-    sns.histplot(resample_errors, kde=True, label='Bootstrap Distribution', color='red', alpha=0.5, bins=50, stat='density', ax=ax)
+    # plot distribution of population
+    sns.histplot(
+        population,
+        kde=True,
+        label="Population Distribution",
+        color="blue",
+        alpha=0.5,
+        bins=50,
+        stat="density",
+        ax=ax,
+    )
+    ax.axvline(np.median(population), color="black", linestyle="--", label="Population Median")
     ax.legend()
-    ax.set_xlabel('Error Value')
-    ax.set_ylabel('Frequency')
-    ax.set_title(f'Distribution of Error Values')
-    print(f'normality pval of real error     : {stats.shapiro(error_list)[1]}')
-    print(f'normality pval of bootstrap error: {stats.shapiro(resample_errors)[1]}')
+    ax.set_xlabel("Value")
+    ax.set_ylabel("Frequency")
+    ax.set_title(f"Distribution of Population")
 
     ax = axes[1]
+    nbins = 20
+    sns.histplot(
+        error_list,
+        kde=True,
+        label="Real Distribution",
+        color="blue",
+        alpha=0.5,
+        bins=nbins,
+        stat="density",
+        ax=ax,
+    )
+    sns.histplot(
+        resample_errors,
+        kde=True,
+        label="Bootstrap Distribution",
+        color="red",
+        alpha=0.5,
+        bins=nbins,
+        stat="density",
+        ax=ax,
+    )
+    sns.histplot(
+        normal_bs_errors,
+        kde=True,
+        label="Normal Distribution",
+        color="green",
+        alpha=0.5,
+        bins=nbins,
+        stat="density",
+        ax=ax,
+    )
+    # sns.histplot(
+    #     scipy_bs_errors,
+    #     kde=True,
+    #     label="Scipy Bootstrap Distribution",
+    #     color="purple",
+    #     alpha=0.5,
+    #     bins=nbins,
+    #     stat="density",
+    #     ax=ax,
+    # )
+    ax.axvline(0, color="black", linestyle="--", label="Zero Error")
+
+    ax.legend()
+    ax.set_xlabel("Error Value")
+    ax.set_ylabel("Frequency")
+    ax.set_title(f"Distribution of Error Values")
+    print(f"normality pval of real error     : {stats.shapiro(error_list)[1]}")
+    print(f"normality pval of bootstrap error: {stats.shapiro(resample_errors)[1]}")
+    # print(f"normality pval of scipy bs  error: {stats.shapiro(scipy_bs_errors)[1]}")
+
+    ax = axes[2]
     # plot Q-Q plot of error_list and resample_errors
-    import statsmodels.api as sm
-    sm.qqplot_2samples(error_list, resample_errors, line='45', ax=ax)
-    ax.set_title('Q-Q Plot of Error Values')
-    ax.set_xlabel('Real Distribution')
-    ax.set_ylabel('Bootstrap Distribution')
+    sm.qqplot_2samples(error_list, resample_errors, line="45", ax=ax)
+    ax.set_title("Q-Q Plot of Error Values")
+    ax.set_xlabel("Real Distribution")
+    ax.set_ylabel("Bootstrap Distribution")
+
+    ax = axes[3]
+    # plot Q-Q plot of error_list and normal_bs_errors
+    sm.qqplot_2samples(error_list, normal_bs_errors, line='45', ax=ax)
+    ax.set_title("Q-Q Plot of Error Values")
+    ax.set_xlabel("Real Distribution")
+    ax.set_ylabel("Normal Bootstrap Distribution")
 
     # Perform the Kolmogorov-Smirnov test
     ks_statistic, p_value = stats.ks_2samp(error_list, resample_errors)
-    print(f'KS statistic: {ks_statistic}, p-value: {p_value}')
+    print(f"KS statistic: {ks_statistic}, p-value: {p_value}")
+    ks_statistic, p_value = stats.ks_2samp(error_list, normal_bs_errors)
+    print(f"KS statistic: {ks_statistic}, p-value: {p_value}")
+    # ks_statistic, p_value = stats.ks_2samp(error_list, scipy_bs_errors)
+    # print(f"KS statistic: {ks_statistic}, p-value: {p_value}")
 
     fig_dir = "/home/ckchang/ApproxInfer/revision/cache/figs"
     save_dir = os.path.join(fig_dir, "2.1.1")
     os.makedirs(save_dir, exist_ok=True)
-    plt.savefig(os.path.join(save_dir, 'error_dist.png'))
-    plt.savefig('./cache/median_error_dist.png')
+    fig_path = os.path.join(save_dir, "median_error_dist.pdf")
+    plt.savefig(fig_path)
+    print(f"Figure saved at {fig_path}")
+    plt.savefig("./cache/median_error_dist.png")
